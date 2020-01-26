5377d18ff079d56cc26baa952bf9c11804aa4020
Oliver Feher <olcsee@gmail.com>
2020-01-26 04:37:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.scan/^(a|e|i|o|u).*\1$/
+  if word.match(/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
